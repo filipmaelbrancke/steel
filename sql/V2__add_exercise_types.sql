3af375ec2b4967a4e1cefc01e65016b4c53d76d1
Devin Austin <devin.austin@gmail.com>
2015-05-18 11:02:32 -0600

    added initial exercise_type seed data

---
+++ sql/V2__add_exercise_types.sql
@@ -0,0 +1,5 @@
+insert into exercise_type(name, description, created_at) values
+('lift', 'resistance training with weights', now()),
+('running', 'distance running', now()),
+('HIIT', 'high intensity interval training', now()),
+('bodyweight', 'bodyweight exercises', now());
