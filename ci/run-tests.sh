a15aa0cee67f45d9c714bf80374e2ceefd6365a2
Devin Austin <devin.austin@gmail.com>
2015-05-19 15:23:07 -0600

    updated to drop tables if tests are being run locally

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,2 +1,6 @@
+if [ "$STEEL_TEST_LOCAL" == "1" ]; then 
+  echo "DROPPING DBs"; 
+  psql -Upostgres -c "drop table exercise; drop table person; drop table exercise_type; drop table schema_history ;" -d steel_test; 
+fi
 sbt -Dflyway.user=steel_test -Dflyway.password=test -Dflyway.url=jdbc:postgresql:steel_test flywayMigrate
 sbt +test
