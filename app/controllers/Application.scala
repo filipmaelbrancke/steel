7b6de9af7656957ee7ab4119273337284c1d6e7c
Devin Austin <devin.austin@gmail.com>
2015-05-21 12:39:02 -0600

    displaying list of workouts works, adding workouts from form works

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,18 +5,61 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.data.format.Formats._
+import org.joda.time.DateTime
+
 import java.util.concurrent.TimeoutException
+
+import models.Exercise
 import dao.PeopleDAO
+import dao.ExercisesDAO
+import dao.ExerciseTypesDAO
+
+import play.api.i18n.Messages.Implicits._
+import Play.current
+import play.twirl.api.Content
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import dao.ExercisesDAO
 
 object Application extends Controller {
-  def exerciseDao = new ExercisesDAO
+  def exerciseDao     = new ExercisesDAO
+  def exerciseTypeDao = new ExerciseTypesDAO
 
-  def index = Action.async {
+  val Home = Redirect(routes.Application.index)
+
+  def index = Action.async { implicit request => 
     val workouts = exerciseDao.findLast(10)
     workouts.map(ex => Ok(views.html.index(ex)))
+  }
+
+  val workoutForm = Form(
+    mapping(
+      "id"        -> longNumber,
+      "kind"      -> longNumber,
+      "reps"      -> optional(longNumber),
+      "weight"    -> optional(of(floatFormat)),
+      "time"      -> optional(of(floatFormat)),
+      "notes"     -> optional(text),
+      "person"    -> longNumber,
+      "createdAt" -> optional(jodaDate),
+      "updatedAt" -> optional(jodaDate))(Exercise.apply)(Exercise.unapply))
+
+  def create = Action.async { implicit rs =>
+    exerciseTypeDao.options.map(options => Ok(html.createForm(workoutForm, options)))
+  }
+
+  def save = Action.async { implicit rs => 
+    Logger.debug("save action hit")
+
+    workoutForm.bindFromRequest.fold(
+      formWithErrors => exerciseTypeDao.options.map(options => BadRequest(html.createForm(formWithErrors, options))),
+      exercise => {
+        for {
+          _ <- exerciseDao.insert(exercise)
+        } yield Home.flashing("success" -> "Exercise added")
+      }
+    )
   }
 
 }
